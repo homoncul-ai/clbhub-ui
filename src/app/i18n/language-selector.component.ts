d08c7d73975dcacc8f22efb179c89d2f95988b5a
Roger Asutin <roger.austin@sematree.com>
2025-05-22 16:45:42 +0530

    lang added

--- src/app/i18n/language-selector.component.ts
+++ src/app/i18n/language-selector.component.ts
@@ -1,12 +1,12 @@
 import { Component, ElementRef, HostListener, Input } from '@angular/core';
 import { I18nService } from './i18n.service';
 import { NgClass, NgForOf } from '@angular/common';
-
+import { MdbDropdownModule } from 'mdb-angular-ui-kit/dropdown';
 @Component({
   selector: 'app-language-selector',
   templateUrl: './language-selector.component.html',
   styleUrls: ['./language-selector.component.scss'],
-  imports: [NgClass],
+  imports: [MdbDropdownModule],
 })
 export class LanguageSelectorComponent {
   @Input() inNavbar = true;
