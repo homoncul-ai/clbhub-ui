7562614ed6acbd68463b22af819e0a546d89dabf
Adam Devoe <adam.devoe@sematree.com>
2025-07-13 11:40:10 -0400

    fx

--- src/app/features/dash-advo/tickets/workrequest-update/workrequest-update.component.ts
+++ src/app/features/dash-advo/tickets/workrequest-update/workrequest-update.component.ts
@@ -1,11 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit, inject } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ActivatedRoute } from '@angular/router';
+import { HcclService } from '@app/restsvc/hccl.service';
+import { WorkRequestCrudWrapper, WorkrequestCrudComponent } from '@app/components/_crud/workrequest-crud/workrequest-crud.component';
+import { SimpleTabsetComponent } from '@app/components/_global/simple-tabset/simple-tabset.component';
+import { CRUD_MODES } from '@app/@core/constants';
 
 @Component({
   selector: 'app-workrequest-update',
-  imports: [],
+  imports: [CommonModule, SimpleTabsetComponent, WorkrequestCrudComponent],
   templateUrl: './workrequest-update.component.html',
   styleUrl: './workrequest-update.component.scss'
 })
-export class WorkrequestUpdateComponent {
+export class WorkrequestUpdateComponent implements OnInit  {
+  @Input() id!: string;
 
+  protected entity!: WorkRequestCrudWrapper ;
+  protected CRUD_MODES = CRUD_MODES;
+  private route = inject(ActivatedRoute);
+  private hcclService = inject(HcclService);
+
+  async ngOnInit(): Promise<void> {
+    this.entity = await WorkRequestCrudWrapper.newInstance(this.id, this.hcclService);
+  }
 }
