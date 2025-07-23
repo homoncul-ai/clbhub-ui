0eca3ad564d52bd5495fe55e695ebd0c783f1f73
Roger Asutin <roger.austin@sematree.com>
2025-07-23 18:02:40 +0530

    removed NG_VALUE_ACCESSOR

--- src/app/components/_global/available-selector/available-selector.component.ts
+++ src/app/components/_global/available-selector/available-selector.component.ts
@@ -1,6 +1,6 @@
 import { Component, Input, Output, EventEmitter, forwardRef } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule, ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-available-selector',
@@ -8,15 +8,8 @@ import { FormsModule, ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/f
   imports: [CommonModule, FormsModule],
   templateUrl: './available-selector.component.html',
   styleUrl: './available-selector.component.scss',
-  providers: [
-    {
-      provide: NG_VALUE_ACCESSOR,
-      useExisting: forwardRef(() => AvailableSelectorComponent),
-      multi: true
-    }
-  ]
 })
-export class AvailableSelectorComponent implements ControlValueAccessor {
+export class AvailableSelectorComponent  {
   // This component is used to display a property called available commonly used in 
   // the hccl.service.ts file. 
   // if available is 1 or greater, then check the checkbox
@@ -24,43 +17,22 @@ export class AvailableSelectorComponent implements ControlValueAccessor {
   // if available is a boolean display as if it's 1 or 0
   // Have a function passed in to handle the change of the checkbox
   // similar to the menu-control-data-list component
-
+  @Input() available: number | boolean = 0;
   @Input() disabled: boolean = false;
   @Input() label: string = 'Available';
   @Input() readonly: boolean = false;
   
   @Output() availableChange = new EventEmitter<number>();
   
-  private _available: number | boolean = 0;
-  private onChange = (value: number) => {};
-  private onTouched = () => {};
   
   uniqueId = 'available-' + Math.random().toString(36).substr(2, 9);
-  
-  get available(): number | boolean {
-    return this._available;
-  }
-  
-  set available(value: number | boolean) {
-    this._available = value;
-    const numericValue = this.convertToNumeric(value);
-    this.onChange(numericValue);
-    this.availableChange.emit(numericValue);
-  }
+
   
   get isChecked(): boolean {
-    console.log(this._available + ' ' + this.readonly);
-    if (typeof this._available === 'boolean') {
-      return this._available;
+    if (typeof this.available === 'boolean') {
+      return this.available;
     }
-    return this._available >= 1;
-  }
-  
-  private convertToNumeric(value: number | boolean): number {
-    if (typeof value === 'boolean') {
-      return value ? 1 : 0;
-    }
-    return value;
+    return this.available >= 1;
   }
   
   onCheckboxChange(event: any): void {
@@ -69,24 +41,6 @@ export class AvailableSelectorComponent implements ControlValueAccessor {
     }
     const isChecked = event.target.checked;
     const newValue = isChecked ? 1 : 0;
-    this.available = newValue;
-    this.onTouched();
-  }
-
-  // ControlValueAccessor implementation
-  writeValue(value: number | boolean): void {
-    this._available = value || 0;
-  }
-
-  registerOnChange(fn: (value: number) => void): void {
-    this.onChange = fn;
-  }
-
-  registerOnTouched(fn: () => void): void {
-    this.onTouched = fn;
-  }
-
-  setDisabledState(isDisabled: boolean): void {
-    this.disabled = isDisabled;
+    this.availableChange.emit(newValue);
   }
 }
