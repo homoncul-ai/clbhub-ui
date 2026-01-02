800f07eae3bd74ccbebe25db23688a90a37bf8a6
Adam Devoe <adam.devoe@sematree.com>
2026-01-02 18:23:53 -0500

    Fx

--- src/app/shell/services/menu.service.ts
+++ src/app/shell/services/menu.service.ts
@@ -1529,7 +1529,7 @@ EA_ORGNONPROFITS_LIST: {
   },
   STUDENT_PERSONALSTATEMENTS: {
     level: 1,
-    label: 'Career Goals',
+    label: 'Personal Goals',
     route: '/student-dashboard/personalstatements',
     componentPath: 'src/app/features/dash-student',
     componentName: 'dash-student-personalstatements',
