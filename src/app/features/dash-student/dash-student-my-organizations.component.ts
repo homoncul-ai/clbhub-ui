e2488abef0fbbee752495b3f42ae1ad5ec09594e
Adam Devoe <adam.devoe@sematree.com>
2026-03-25 16:57:34 -0400

    Fx

--- src/app/features/dash-student/dash-student-my-organizations.component.ts
+++ src/app/features/dash-student/dash-student-my-organizations.component.ts
@@ -3,12 +3,18 @@ import { CommonModule } from '@angular/common';
 import { MdbAccordionModule } from 'mdb-angular-ui-kit/accordion';
 import { HcclContextService } from '@app/shell/services/hccl-context.service';
 import { HcclOrganizationInterestListComponent } from '@app/components/_crud/hcclorganizationinterest/hcclorganizationinterest-list.component';
-import { HcclOrganizationInterestCriteria } from '@app/restsvc/hccl.service';
+import {
+  HcclOrganizationInterestCriteria,
+  SimpleMapEntry,
+  SimpleMapEntryResponse,
+  HcclService,
+} from '@app/restsvc/hccl.service';
+import { MapAddrUiComponent } from '@app/components/_crud/map-addr-ui/map-addr-ui.component';
 
 @Component({
   selector: 'app-dash-student-my-organizations',
   standalone: true,
-  imports: [CommonModule, MdbAccordionModule, HcclOrganizationInterestListComponent],
+  imports: [CommonModule, MdbAccordionModule, HcclOrganizationInterestListComponent, MapAddrUiComponent],
   template: `
     <div class="container-fluid">
       <div class="row">
@@ -45,6 +51,22 @@ import { HcclOrganizationInterestCriteria } from '@app/restsvc/hccl.service';
                 </app-hcclorganizationinterest-list>
               </ng-template>
             </mdb-accordion-item>
+
+            <mdb-accordion-item
+              [collapsed]="isAccordionCollapsed('map')"
+              (itemShow)="openAccordion('map')">
+              <ng-template mdbAccordionItemHeader>
+                <i class="fas fa-map-location-dot me-2"></i>
+                2. Organization Map
+              </ng-template>
+              <ng-template mdbAccordionItemBody>
+                <app-map-addr-ui
+                  title="My Organizations Map"
+                  [mapEntryResponse]="organizationMapEntryResponse || undefined"
+                  (pinClick)="onOrganizationMapPinClick($event)">
+                </app-map-addr-ui>
+              </ng-template>
+            </mdb-accordion-item>
           </mdb-accordion>
         </div>
       </div>
@@ -53,11 +75,13 @@ import { HcclOrganizationInterestCriteria } from '@app/restsvc/hccl.service';
 })
 export class DashStudentMyOrganizationsComponent implements OnInit {
   private hcclContextService = inject(HcclContextService);
+  private hcclService = inject(HcclService);
 
   accordionId = 'organizations';
   loading = true;
   error = '';
   private userProfileId = '';
+  protected organizationMapEntryResponse: SimpleMapEntryResponse | null = null;
 
   ngOnInit(): void {
     this.hcclContextService.waitForReady$().subscribe({
@@ -65,11 +89,15 @@ export class DashStudentMyOrganizationsComponent implements OnInit {
         this.userProfileId = context?.currentUserProfileId || '';
         if (!this.userProfileId) {
           this.error = 'User profile context was not found.';
+          this.organizationMapEntryResponse = { searchResults: [] };
+        } else {
+          this.updateMapEntries();
         }
         this.loading = false;
       },
       error: () => {
         this.error = 'Unable to resolve user context.';
+        this.organizationMapEntryResponse = { searchResults: [] };
         this.loading = false;
       },
     });
@@ -90,5 +118,20 @@ export class DashStudentMyOrganizationsComponent implements OnInit {
       pageSize: 50,
       isPaging: true,
     };
+  }
+
+  private updateMapEntries(): void {
+    this.hcclService.lookupRelatedMapEntry('HcclUserProfile', this.userProfileId, 'HcclOrganizationInterest').subscribe({
+      next: (searchResults: SimpleMapEntryResponse) => {
+        this.organizationMapEntryResponse = searchResults;
+      },
+      error: () => {
+        this.organizationMapEntryResponse = { searchResults: [] };
+      },
+    });
+  }
+
+  onOrganizationMapPinClick(entry: SimpleMapEntry): void {
+    console.log('My organizations map pin clicked:', entry);
   }
 }
