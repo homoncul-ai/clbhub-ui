37d7f2493334f0989c02e942495ed3bc5560b59e
Cole Flather <coltonflather@gmail.com>
2026-07-03 12:08:37 -0400

    Got rid of thing in localhost script for limiting memory(needed on my device)

--- run_on_localhost.sh
+++ run_on_localhost.sh
@@ -61,7 +61,7 @@ then
     echo "${appName} starting up on : http://localhost:${port}/ "
 
     echo ng serve
-    export NODE_OPTIONS="${NODE_OPTIONS:-} --max-old-space-size=8192"
+    export NODE_OPTIONS="${NODE_OPTIONS:-}
     ng serve
 
 else
